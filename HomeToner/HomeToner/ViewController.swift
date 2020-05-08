a7b608eeb93540064f26a0ca09e6e0596c9dfdc9
Antonio de Jesús Hernádez Ruiz <54318030+AntonioHernandez28@users.noreply.github.com>
2020-05-08 16:36:03 -0500

    Add files via upload

---
+++ HomeToner/HomeToner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeToner
+//
+//  Created by Antonio Hernández Ruiz on 06/05/20.
+//  Copyright © 2020 Antonio Hernández Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeToner/HomeToner/ViewControllerOrdenar.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerOrdenar.swift
+//  HomeToner
+//
+//  Created by Antonio Hernández Ruiz on 06/05/20.
+//  Copyright © 2020 Antonio Hernández Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerOrdenar: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
